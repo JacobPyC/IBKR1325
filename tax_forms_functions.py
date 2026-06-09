62bcbd0c787abd8e66447dc588476c6150a9ee2e
JacobC <jacobc6569@gmail.com>
2026-06-09 21:12:18 +0300

    ADD YEAR PROFIT

--- tax_forms_functions.py
+++ tax_forms_functions.py
@@ -610,9 +610,10 @@ def _write_cg_summary_table(sheet, h1_profit, h1_loss, h1_sell, h2_profit, h2_lo
         (9,  'רווח מחצית שנייה',             round_half_up(h2_profit),                                      H2_FILL,    DATA_FONT),
         (10, 'הפסד מחצית שנייה',             round_half_up(abs(h2_loss)),                                   H2_FILL,    DATA_FONT),
         (11, 'סכום מכירות מחצית שנייה',      round_half_up(h2_sell),                                        H2_FILL,    DATA_FONT),
-        (12, 'הפסד כולל שנתי',               round_half_up(abs(h1_loss + h2_loss)),                         YEAR_FILL,  DATA_FONT),
-        (13, 'מכירות שנתי',                  round_half_up(h1_sell + h2_sell),                              YEAR_FILL,  DATA_FONT),
-        (14, 'רווח שנתי לאחר קיזוז הפסדים', round_half_up((h1_profit + h2_profit) + (h1_loss + h2_loss)), YEAR_FILL,  DATA_FONT),
+        (12, 'רווח כולל שנתי',               round_half_up(h1_profit + h2_profit),                          YEAR_FILL,  DATA_FONT),
+        (13, '(56/256)הפסד כולל שנתי',      round_half_up(abs(h1_loss + h2_loss)),                         YEAR_FILL,  DATA_FONT),
+        (14, 'מכירות שנתי',                  round_half_up(h1_sell + h2_sell),                              YEAR_FILL,  DATA_FONT),
+        (15, 'רווח שנתי לאחר קיזוז הפסדים', round_half_up((h1_profit + h2_profit) + (h1_loss + h2_loss)), YEAR_FILL,  DATA_FONT),
     ]
 
     for row_num, label, value, fill, font in rows:
